4f74dab55d932badf6dbd431ac775d9ddd4b4308
whchoi98 <whchoi98@gmail.com>
2026-04-04 03:06:10 +0000

    feat: add structlog JSON logging setup

---
+++ src/common/logging.py
@@ -0,0 +1,23 @@
+"""Structured logging setup using structlog."""
+import structlog
+
+
+def setup_logging() -> None:
+    structlog.configure(
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.processors.add_log_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.processors.JSONRenderer(),
+        ],
+        wrapper_class=structlog.make_filtering_bound_logger(0),
+        context_class=dict,
+        logger_factory=structlog.PrintLoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
+
+
+def get_logger(name: str) -> structlog.stdlib.BoundLogger:
+    return structlog.get_logger(name)
